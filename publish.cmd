627e3ff565fe22c182a2a6bd97febb824c29ca38
johndcastro <johndcastro@gmail.com>
2018-08-08 08:53:30 -0500

    Initial Load
    John Castro

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=hcctravelassistant-Web-Deploy.pubxml -p:Password=BmwpfiddXw8wgztM0mtrn0R79lyAPihthR3xLZtLqniSKRjHdmkKmNlHojyp
+
